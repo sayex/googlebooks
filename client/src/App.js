eb74238f6d2c79c8478e81a7bcb4ff8ce117ecd0
sayex <elsayer@msn.com>
2019-04-04 14:13:06 -0600

    added .map for results in state

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import Books from "./pages/Books";
 
 
--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -2,6 +2,8 @@
 import React, {Component} from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Axios from "axios"
+import { List, ListItem } from "../components/List";
+import { Link } from "react-router-dom";
 
 
 class Books extends Component {
@@ -19,7 +21,8 @@ textInput= (event) =>{
 submitSearch= (event) => {
     event.preventDefault();
     Axios.get('https://www.googleapis.com/books/v1/volumes?q='+ this.state.search)
-  .then( (response) =>{
+  .then( (response) => {
+      console.log(response.data.items);
     this.setState({
         books: response.data.items
     })
@@ -29,14 +32,42 @@ submitSearch= (event) => {
   });
 }
 
+booksArray = ()=> {
+
+}
+
     render() {
         return (
           <Container>
-              <form>
-                 <input type ="text" name="search" onChange={this.textInput} />
-                 <button type ="submit" onClick={this.submitSearch}>Search</button>
-              </form>
-
+                <Row>
+                    <Col size="md-6">
+                        <form>
+                            <input type ="text" name="search" onChange={this.textInput} />
+                            <button type ="submit" onClick={this.submitSearch}>Search</button>
+                        </form>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-6">
+                {this.state.books.length ? (
+                    <List>
+                {this.state.books.map((book,i )=> (
+                  <ListItem key={i}>
+                    <Link to={"/"}>
+                      <strong>
+                        {book.volumeInfo.title} by {book.volumeInfo.authors}
+                      </strong>
+                    </Link>
+                    {/* <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+                    </Col>
+                </Row>
+             
           </Container>
         );
     }
